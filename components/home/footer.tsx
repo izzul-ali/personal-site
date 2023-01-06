9c96b78f2026e8ff9b6193fdf53e5a13acc2de91
Izzul Maali <110296589+izzul-ali@users.noreply.github.com>
2023-01-06 09:00:27 +0700

    feat: change font size in footer

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -2,7 +2,7 @@ import SocialMedia from '@/components/social-media'
 
 export default function Footer() {
   return (
-    <footer className="bg-transparent text-sm text-gray-600 border-t border-gray-300 mt-16 pt-3 leading-normal tracking-wide">
+    <footer className="bg-transparent text-xs text-gray-600 border-t border-gray-300 mt-16 pt-3 leading-normal tracking-wide">
       <SocialMedia />
       <p className="mt-7">©️ 2022 Izzul. All rights reserved.</p>
       <p>Built with Next.js and Tailwindcss.</p>
