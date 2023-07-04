079ddeeb7b505bb01eee542c7d1bc02e15ff6b0d
izzul-ali <izzulmaali3@gmail.com>
2023-07-04 14:41:09 +0700

    update year

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -3,15 +3,15 @@ import SocialMedia from '@/components/social-media'
 export default function Footer() {
   return (
     <footer className="bg-transparent text-xs text-gray-600 border-t border-gray-300 mt-16 pt-3 leading-normal tracking-wide">
-      <SocialMedia />
-      <p className="mt-7">©️ 2022 Izzul. All rights reserved.</p>
+      <SocialMedia contact={false} />
+      <p className="mt-7">©️ 2023 Izzul. All rights reserved.</p>
       <p>Built with Next.js and Tailwindcss.</p>
       <p>
         All code for this website is available on{' '}
         <a href="https://github.com/izzul-ali" className="underline">
           github
         </a>
-        {''}.
+        .
       </p>
     </footer>
   )
